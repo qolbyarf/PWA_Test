29aca72e150c837775ddd95ac270d8f230ce1424
qolbyarf <putraendala@gmail.com>
2024-06-30 23:29:02 +0700

    ImagePlugins

--- webpack.common.js
+++ webpack.common.js
@@ -4,54 +4,60 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
-const ImageminMozjpeg = require('imagemin-mozjpeg');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+async function getImageminMozjpeg() {
+  const ImageminMozjpeg = await import('imagemin-mozjpeg');
+  return ImageminMozjpeg.default;
+}
 
-module.exports = {
-  entry: {
-    app: path.resolve(__dirname, 'src/scripts/index.js'),
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new BundleAnalyzerPlugin(),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: path.resolve(__dirname, 'src/templates/index.html'),
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
+module.exports = async () => {
+  const ImageminMozjpeg = await getImageminMozjpeg();
+  return {
+    entry: {
+      app: path.resolve(__dirname, 'src/scripts/index.js'),
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+      publicPath: '/',
+    },
+    module: {
+      rules: [
         {
-          from: path.resolve(__dirname, 'src/public/'),
-          to: path.resolve(__dirname, 'dist/'),
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader',
+          ],
         },
       ],
-    }),
-    new ImageminWebpackPlugin({
-      plugins: [
-        ImageminMozjpeg({
-          quality: 50,
-          progressive: true,
-        }),
-      ],
-      overrideExtension: true,
-    }),
-  ],
+    },
+    plugins: [
+      new BundleAnalyzerPlugin(),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: path.resolve(__dirname, 'src/templates/index.html'),
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'src/public/'),
+            to: path.resolve(__dirname, 'dist/'),
+          },
+        ],
+      }),
+      new ImageminWebpackPlugin({
+        plugins: [
+          ImageminMozjpeg({
+            quality: 50,
+            progressive: true,
+          }),
+        ],
+        overrideExtension: true,
+      }),
+    ],
+  };
 };
