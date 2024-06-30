6696969049ccfdca1599b24fcac412ba9321e77c
azbagas <azharbagaskara1@gmail.com>
2024-06-30 23:32:53 +0700

    fix error

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,20 +1,23 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
- 
-module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    open: true,
-    port: 9000,
-    client: {
-      overlay: {
-        errors: true,
-        warnings: true,
+
+module.exports = (async () => {
+  const common = await import('./webpack.common.js');
+  const commonConfig = await common.default();
+  return merge(commonConfig, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+      static: path.resolve(__dirname, 'dist'),
+      open: true,
+      port: 9000,
+      client: {
+        overlay: {
+          errors: true,
+          warnings: true,
+        },
       },
+      compress: true,
     },
-    compress: true,
-  },
-});
+  });
+})();
